a5069bd966952d70705b02aaec9ab7724bb96fe2
TinaFeng <xuanfeng1996@gmail.com>
2017-04-22 14:21:12 -0700

    Prototype for hunter

---
+++ Assets/Obstacles/Hunter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunter : MonoBehaviour {
+
+    GameObject player;
+
+ 
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+       // sight = this.gameObject.transform.GetChild(0);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("I've got you in my sight.");
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+
+        float angle = 0; //look at 2D
+        Vector3 relative = transform.InverseTransformPoint(player.transform.position);
+        angle = Mathf.Atan2(relative.x, relative.y) * Mathf.Rad2Deg;
+        transform.Rotate(0, 0, -angle);
+    }
+}
---
+++ Assets/Obstacles/Vision.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vision : MonoBehaviour {
+
+    GameObject Parent_Hunter;
+    Vector3 Prey;
+
+    Rigidbody2D rigid;
+    public float hunter_speed = 4000;
+
+    bool chase = false;
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponentInParent<Rigidbody2D>();
+        Parent_Hunter = transform.root.gameObject;
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Prey = other.transform.position;
+            chase = true;
+           // Make parent chase player
+        }
+
+
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+        //chasing Player
+        if(chase)
+        {
+            Vector2 targetDirection = Prey - this.transform.position;
+            targetDirection.Normalize();
+            rigid.velocity = Vector2.MoveTowards(rigid.velocity, hunter_speed*targetDirection, Time.deltaTime *1);
+
+        }
+    }
+}
