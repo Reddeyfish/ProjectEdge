11eba3528e6472b0e277fad8019041d0abc0d69a
Reddeyfish <edrichd@uci.edu>
2017-04-22 12:20:00 -0700

    Add button to wipe saved data

---
+++ Assets/Base/PurchaseScripts/WipePlayerPrefs.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WipePlayerPrefs : MonoBehaviour {
+    
+    public void ButtonWipePlayerPrefs() {
+        PlayerPrefs.DeleteAll();
+    }
+
+}
