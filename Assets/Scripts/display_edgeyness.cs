f3a4a56baec33be148e8d95c640029814fd01fd6
kathermf <kathermf@uci.edu>
2017-04-21 21:53:43 -0700

    Added PlayerEdgeyness script to player, started edgeyness display

---
+++ Assets/Scripts/display_edgeyness.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class display_edgeyness : MonoBehaviour {
+
+    public int edgeyness;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
