ba88338ae7610f41ccb6ac89a12e0f9fe3c09e4d
Jeffrey Lee Ye <jlye@uci.edu>
2017-04-22 16:48:26 -0700

    Added a form of knockback when touching an obstacle

---
+++ Assets/Obstacles/Meteors/MeteorCrashing.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MeteorCrashing : MonoBehaviour {
+
+    private Vector2 previousVelocity;
+
+    protected Rigidbody2D rgdBody;
+    public int damage;
+    public float crashLength;
+
+    [SerializeField]
+    protected float crashStrengthMultiplier = 1;
+
+    [SerializeField]
+    private GameObject playerPushBack;
+
+    private float currentCrashTime = 0;
+    
+    //Player attributes
+    private GameObject player;
+    private Rigidbody2D playerRB;
+
+	// Use this for initialization
+	void Start () {
+        rgdBody = GetComponent<Rigidbody2D>();
+
+        //Get player attributes
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        previousVelocity = rgdBody.velocity;
+    }
+
+    private bool canCrash() {
+        float angleOfVelocity = Mathf.Atan2(rgdBody.velocity.y, rgdBody.velocity.x);
+        float angleBetweenPlayerAndSelf = (Mathf.Atan2(player.transform.position.y - transform.position.y, player.transform.position.x - transform.position.x));
+        angleOfVelocity += angleOfVelocity < 0 ? 2 * Mathf.PI : 0;
+        angleBetweenPlayerAndSelf += angleBetweenPlayerAndSelf < 0 ? 2 * Mathf.PI : 0;
+        angleOfVelocity *= (180 / Mathf.PI);
+        angleBetweenPlayerAndSelf *= (180 / Mathf.PI);
+
+        print(angleOfVelocity + " | " + angleBetweenPlayerAndSelf);
+
+        return Mathf.Abs(angleOfVelocity - angleBetweenPlayerAndSelf) < 90;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            GameObject tempPrefabPushBack = Instantiate(playerPushBack, collision.transform);
+            tempPrefabPushBack.GetComponent<PlayerPushBack>().Init(crashLength, damage, previousVelocity.sqrMagnitude);
+        }
+        previousVelocity = rgdBody.velocity;
+    }
+}
---
+++ Assets/Player/PlayerKnockBack/PlayerPushBack.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPushBack : MonoBehaviour {
+
+    private int damage;
+    private CameraShakeScript camShake;
+    private float crashLength;
+    private float currentVelocity;
+
+    //Player attributes
+    private GameObject player;
+    private Health playerHealth;
+    private PlayerMovement playerMovement;
+    private Rigidbody2D playerRB;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+
+    public void Init(float crashLength, int damage, float currentVelocity) {
+        this.crashLength = crashLength;
+        this.damage = damage;
+        this.currentVelocity = currentVelocity;
+        print(currentVelocity);
+
+        camShake = Camera.main.GetComponent<CameraShakeScript>();
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<Health>();
+        playerMovement = player.GetComponent<PlayerMovement>();
+        playerRB = player.GetComponent<Rigidbody2D>();
+
+        StartCoroutine(crashPlayer());
+    }
+
+    protected IEnumerator crashPlayer() {
+        float currentCrashTime = 0;
+
+        playerHealth.Damage(damage);
+        playerMovement.enabled = false;
+
+        camShake.screenShake(crashLength / 2);
+
+        Vector2 direction = (player.transform.position - transform.position).normalized;
+        
+        playerRB.velocity = direction * currentVelocity;
+
+        while (currentCrashTime < crashLength) {
+
+            currentCrashTime += Time.deltaTime;
+            print(string.Format("Getting crashed"));
+            yield return 0;
+        }
+        //yield return new WaitForSeconds(crashLength);
+        
+        playerMovement.enabled = true;
+
+        Destroy(gameObject);
+    }
+}
