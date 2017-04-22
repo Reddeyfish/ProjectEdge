a8f7cf592e65d4506e4cc748470e201fd1c9f019
kathermf <kathermf@uci.edu>
2017-04-22 12:42:01 -0700

    displaying edgeyness fixing and cleanup

--- Assets/Scripts/UI/display_edgeyness.cs
+++ Assets/Scripts/UI/display_edgeyness.cs
@@ -4,28 +4,52 @@ using UnityEngine;
 
 public class display_edgeyness : MonoBehaviour {
 
-    public int edgeyness = 0;
-    public GameObject player;
-    public GameObject start;
-    public GameObject bar;
-    public GameObject display;
+    // ui script for displaying the amount of edgeyness
 
-    private Vector3 pos;
-    private static int max_edgeyness = 100;
-    private GameObject[] bar_array = new GameObject[max_edgeyness];
+    // debugging
+    public bool debug = false;
+    public int edgeyness = 0;
+
+    public bool isTemp = false;
+
+    // pls insert this stuff in inspector
+    public GameObject player;
+    public GameObject start; // obj w/ starting pos of bars
+    public GameObject next;
+    public GameObject bar; // prefab for the bar that appears as edgeyness
+    //public GameObject display; 
+
+    private Vector3 pos; // position of start, used for start pos of bars
+    private Vector3 scale;
+    private static int max_edgeyness = 80; // number of bars to instantiate
+    private GameObject[] bar_array ; // array of bars :V
+
+    private float offset = -20;
 
 	// Use this for initialization
 	void Start () {
         player = GameObject.Find("Player");
         pos = start.transform.position;
+        scale = start.transform.localScale;
 
+        offset = next.transform.position.x - pos.x;
+
+        // set up bar array
+        if (!debug) 
+        {
+            max_edgeyness = PlayerEdgeyness.getMaxTempEdgeyness();
+        }
+        bar_array = new GameObject[max_edgeyness];
+
+        // instantiate bars
         for (int i = 0; i < max_edgeyness; i++)
         {
-            // disp: x + (i * -10)
-            Vector3 newpos = new Vector3(pos.x + (i * -20), pos.y, pos.z);
+            // disp: x + (i * -10) <- adds offset to each bar pos so that they don't display on top of eachother
+            Vector3 newpos = new Vector3(pos.x + (i * offset), pos.y, pos.z);
             GameObject newbar = Instantiate(bar, newpos, start.transform.rotation);
-            newbar.transform.SetParent(display.transform);
-            newbar.SetActive(false);
+            newbar.transform.SetParent(this.gameObject.transform); // put them under the edgeyness obj
+            newbar.transform.localScale = new Vector3(scale.x, scale.y, scale.z);
+            newbar.SetActive(false); // make them all invisible
             //newbar.transform.localScale = new Vector3(0, 0, 0);
             bar_array[i] = newbar;
 
@@ -37,17 +61,22 @@ public class display_edgeyness : MonoBehaviour {
         // turn off all bars
         foreach (GameObject b in bar_array) 
         {
-            //b.transform.localScale = new Vector3(0, 0, 0);
             b.SetActive(false);
+            //b.transform.localScale = new Vector3(0, 0, 0);
         }
 
-
 		// get edgeyness amount
-        // v uncomment this before using
-        //edgeyness = player.GetComponent<PlayerEdgeyness>().getEdgeyness();
-
-        // turn on bars
-
+        if (!debug)
+        {
+            if (!isTemp)
+            {
+                edgeyness = PlayerEdgeyness.getEdgeyness();
+            }
+            else {
+                edgeyness = PlayerEdgeyness.getTempEdgeyness();
+            }
+        }
+        // turn on necessary bars
         for (int i = 0; i < edgeyness && i < max_edgeyness; i++) 
         {
             bar_array[i].SetActive(true);
