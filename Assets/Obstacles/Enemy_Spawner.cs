a92eb8b0c025b2e74c9f5302441c9897bcd1155c
TinaFeng <xuanfeng1996@gmail.com>
2017-04-21 22:32:17 -0700

    Basic Enemy spawner and despawner

---
+++ Assets/Obstacles/Enemy_Spawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Spawner : MonoBehaviour {
+
+    public GameObject prefab;
+    GameObject player;
+    public int numberOfObjects = 20;
+    public float radius = 10f;
+    Vector3 player_position;
+    // Use this for initialization
+    void Start()
+    {
+      player = GameObject.FindGameObjectWithTag("Player");
+
+
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+        int count = obstacles.Length;
+
+        player_position = new Vector3(player.transform.position.x, player.transform.position.y, 0);
+
+
+
+        if (count < numberOfObjects)
+        {
+            Debug.Log("Too Little,Spawn more edge.");
+            for (int i = 0; i <= numberOfObjects; i++)
+                
+            {
+                Vector3 pos = new Vector3(Random.Range(player_position.x - radius, player_position.x + radius),
+                    Random.Range(player_position.y - radius, player_position.y + radius), 0);
+                Instantiate(prefab, pos, Quaternion.identity);
+            }
+        }
+        
+    }
+}
---
+++ Assets/Obstacles/Obstacle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+    GameObject player;
+
+    public float detection_range = 30f;
+    private void Start()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("Bump");
+        }
+    }
+
+
+   // Update is called once per frame
+       void Update()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        float distance = Vector3.Distance(this.transform.position, player.transform.position);
+        Debug.Log(distance);
+        if (distance > detection_range)
+            Destroy(gameObject);
+    }
+}
