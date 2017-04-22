168e0ec94d31370444e586345f5f6e7dba264bad
Reddeyfish <edrichd@uci.edu>
2017-04-21 21:34:39 -0700

    Create Simple Base

---
+++ Assets/Base/BaseEntry.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEntry : MonoBehaviour {
+
+    [SerializeField]
+    protected Canvas baseScreen;
+
+    [SerializeField]
+    protected Canvas baseTooltip;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        baseTooltip.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        baseTooltip.gameObject.SetActive(false);
+    }
+
+    private void Update() {
+        if(baseTooltip.isActiveAndEnabled) {
+            if(Input.GetKeyDown(KeyCode.Space)) {
+                baseScreen.gameObject.SetActive(true);
+            }
+        }
+    }
+}
