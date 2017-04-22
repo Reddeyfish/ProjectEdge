fa01d031aec3eeba62d4173b30da00548fc93858
Reddeyfish <edrichd@uci.edu>
2017-04-22 13:55:33 -0700

    Add chain leech gun

---
+++ Assets/Player/Weapon/ChainLeech.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChainLeech : MonoBehaviour {
+
+    [SerializeField]
+    protected float initialDamage = 5f; //seconds of DoT, applied immediately
+
+    Rigidbody2D rigid;
+    SpringJoint2D attachJoint;
+    LineRenderer rend;
+    Transform target;
+    Vector3 attachPoint;
+    Health targetHealth;
+
+    float damageScale;
+
+    private void Awake() {
+        rigid = GetComponent<Rigidbody2D>();
+        attachJoint = GetComponent<SpringJoint2D>();
+        rend = GetComponent<LineRenderer>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        rend.SetPosition(1, transform.InverseTransformPoint(target.TransformPoint(attachPoint)));
+        targetHealth.Damage(damageScale * Time.deltaTime);
+	}
+
+    public void Init(RaycastHit2D hit, float damageScale) {
+        this.damageScale = damageScale;
+
+        this.target = hit.transform;
+        attachPoint = target.InverseTransformPoint(hit.point);
+        attachJoint.connectedBody = target.GetComponentInParent<Rigidbody2D>();
+        attachJoint.connectedAnchor = attachPoint;
+        attachJoint.distance = hit.distance;
+
+        targetHealth = target.GetComponentInParent<Health>();
+        targetHealth.onDeath += TargetHealth_onDeath;
+        targetHealth.Damage(damageScale * initialDamage);
+
+        rigid.velocity = 5 * Random.insideUnitCircle;
+    }
+
+    private void TargetHealth_onDeath() {
+        attachJoint.connectedBody = null;
+        SimplePool.Despawn(transform.root.gameObject);
+        targetHealth.onDeath -= TargetHealth_onDeath;
+    }
+}
---
+++ Assets/Player/Weapon/LeechGun.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeechGun : MonoBehaviour {
+
+    PlayerMovement input;
+    LayerMask mask;
+
+    [SerializeField]
+    protected GameObject leechPrefab;
+
+    public float range = 10f;
+
+    public float damageScale = 10.0f;
+
+	// Use this for initialization
+	void Awake () {
+        input = GetComponentInParent<PlayerMovement>();
+        mask = LayerMask.GetMask(Tags.Layers.Enemy);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!Input.GetMouseButtonDown(0)) { return; }
+        Vector2 aimingDirection = input.rawAimingInput.normalized;
+
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, aimingDirection, range, mask);
+        if (!hit) { return; }
+
+        GameObject leech = SimplePool.Spawn(leechPrefab, transform.position);
+        leech.GetComponent<ChainLeech>().Init(hit, damageScale);
+
+        //health cost
+
+        //reload
+	}
+}
