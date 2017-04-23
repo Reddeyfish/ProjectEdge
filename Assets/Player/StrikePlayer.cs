b5fc79daff1fc1086f619c772c2d47d9f027a366
Jeffrey Lee Ye <jlye@uci.edu>
2017-04-22 23:48:38 -0700

    VariableName change to make it sound clearer

--- Assets/Player/StrikePlayer.cs
+++ Assets/Player/StrikePlayer.cs
@@ -7,6 +7,8 @@ public class StrikePlayer : MonoBehaviour {
     [SerializeField]
     public GameObject playerPushBack;
     
+	public void Strike(Transform playerTransform, float crashLength, int damage, Vector2 velocityPower, Vector2 currentPos) {
+        GameObject tempPrefabPushBack = Instantiate(playerPushBack, playerTransform);
         tempPrefabPushBack.GetComponent<PlayerPushBack>().Init(crashLength, damage, velocityPower, currentPos);
     }
 
