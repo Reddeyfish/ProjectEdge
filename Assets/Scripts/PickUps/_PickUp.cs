a2876270e7bcf65d0dded37678b85fcf7df9c5c5
Reddeyfish <edrichd@uci.edu>
2017-04-23 12:33:16 -0700

    Add visuals to pickups

--- Assets/Scripts/PickUps/_PickUp.cs
+++ Assets/Scripts/PickUps/_PickUp.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class _PickUp : MonoBehaviour {
 
+    [SerializeField]
+    protected Sprite[] possibleSprites;
+
     protected GameObject attributesObject;
 	// Use this for initialization
 	void Start () {
         attributesObject = transform.GetChild(0).gameObject;
+        SpriteRenderer spriteRend = GetComponent<SpriteRenderer>();
+        spriteRend.sprite = possibleSprites[Random.Range(0, possibleSprites.Length)];
 	}
 	
     void OnTriggerEnter2D(Collider2D collided) {
