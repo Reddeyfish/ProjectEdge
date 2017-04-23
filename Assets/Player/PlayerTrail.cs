5a61d5376fd6defa14f4bf0be21d91f6e94948cc
TinaFeng <xuanfeng1996@gmail.com>
2017-04-23 14:20:31 -0700

    Dah Edgewin Quotes!

---
+++ Assets/Player/PlayerTrail.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.IO;
+
+public class PlayerTrail : MonoBehaviour {
+
+    public TextAsset text;
+    List<string> trails;
+
+    public float movement_speed = 10f;
+
+    public float timer = 5f;
+    float time = 0;
+    // Use this for initialization
+    void Start () {//text things
+        trails = new List<string>();
+        StringReader reader = new StringReader(text.text);
+        if (reader != null)
+        {
+            string line = reader.ReadLine();
+            string[]quotes = line.Split(';');
+            for (int i = 0; i != quotes.Length - 1; i++)
+                trails.Add(quotes[i]);
+           
+        }
+
+        this.GetComponent<TextMesh>().text = trails[Random.Range(0, trails.Count)];
+        this.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-3, 3), Random.Range(-3, 3)) * movement_speed);
+    }
+	
+
+	// Update is called once per frame
+	void Update () {
+
+        if (time < timer)
+        {
+            time += Time.deltaTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+
+
+    }
+
+   
+}
---
+++ Assets/Player/TrailSpawn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailSpawn : MonoBehaviour {
+
+    public GameObject quotes;
+
+    GameObject player;
+    Vector3 player_position;
+
+
+
+    public float timer = 3f;
+    float current_time;
+    // Use this for initialization
+    void Start () {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        player_position = new Vector3(player.transform.position.x, player.transform.position.y, 0);
+        spawn_text();
+    }
+    void spawn_text()
+    {
+            if (current_time < timer)
+            {
+                current_time += Time.deltaTime;
+            }
+
+            else
+            {
+                current_time = 0;
+                //Debug.Log("Too Little,Spawn more edge.");
+                Vector3 pos = new Vector3(player_position.x,player_position.y, 0);
+
+                Instantiate(quotes, pos, Quaternion.identity);
+
+
+            }
+
+        }
+    }
