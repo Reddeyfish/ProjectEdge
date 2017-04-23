b3b6c84b13bf0706c8e55651fa182ba3667a744e
Reddeyfish <edrichd@uci.edu>
2017-04-23 14:51:47 -0700

    Add sound when player hit

---
+++ Assets/Player/SoundOnHit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnHit : MonoBehaviour {
+
+    AudioSource audioSource;
+
+    [SerializeField]
+    protected float threshold = 5;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        GetComponent<Health>().onDamage += SoundOnHit_onDamage;
+
+    }
+
+    private void SoundOnHit_onDamage(float amount) {
+        if (amount > threshold) {
+            audioSource.Play();
+        }
+    }
+}
