07e0bcf620f07b5bacd3bad5d89eec81ed58f58f
TinaFeng <xuanfeng1996@gmail.com>
2017-04-23 18:26:31 -0700

    Title Screen

---
+++ Assets/Scripts/UI/LoadOnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour
+{
+
+    //currently 0 is title , 1 is start, 2 is how to play, 3 is quit
+
+    public void LoadScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
